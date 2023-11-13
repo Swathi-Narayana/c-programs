481956306ae8f534ce095c2f879a467b7aa233a3
Swathi-Narayana <149993249+Swathi-Narayana@users.noreply.github.com>
2023-11-13 14:54:47 +0530

    GenericClass

--- constructors.cs
+++ constructors.cs
@@ -2,19 +2,49 @@
 
 namespace HelloWorld
 {
-    class Car
+    public class GFG<T>
     {
-        string model;
-        public Car()
-        {
-            model = "Mustang";
-        }
 
-        static void Main(string[] args)
+        // private data members
+        private T data;
+
+        // using properties
+        public T value
         {
-            Car Ford = new Car();
-            Console.WriteLine(Ford.model);
+
+            // using accessors
+            get
+            {
+                return this.data;
+            }
+            set
+            {
+                this.data = value;
+            }
         }
     }
 
+    // Driver class
+    class Test
+    {
+
+        // Main method
+        static void Main(string[] args)
+        {
+
+            // instance of string type
+            GFG<string> name = new GFG<string>();
+            name.value = "GeeksforGeeks";
+
+            // instance of float type
+            GFG<float> version = new GFG<float>();
+            version.value = 5.0F;
+
+            // display GeeksforGeeks
+            Console.WriteLine(name.value);
+
+            // display 5
+            Console.WriteLine(version.value);
+        }
+    }
 }
